20d29be108fb2ee10945c620fee093fb225c95b7
omarmorle <omarmorleno@gmail.com>
2022-11-23 20:42:41 -0600

    Constancia ipn

---
+++ resources/views/generar-constancia-externa.blade.php
@@ -0,0 +1,31 @@
+@php
+    //Obtener dia actual
+    $dia = date('d');
+    //Obtener mes actual
+    $mes = date('m');
+    //Obtener el año actual
+    $anio = date('Y');
+    //Obtener los datos de la base de datos de la tabla asistencias con el id
+    $asistencia = DB::table('asistencias')->where('id', $id)->first();
+    //Obtener el evento de la tabla eventos con el id del evento de la tabla asistencias
+    $evento = DB::table('eventos')->where('id', $asistencia->idEvento)->first();
+    //Obtener el club con el id_club de la tabla eventos
+    $club = DB::table('clubes')->where('id', $evento->id_club)->first();
+    //Obtener los datos del usuario con el idUsuario de la tabla asistencias
+    $usuario = DB::table('users')->where('id', $asistencia->idUsuario)->first();
+    
+@endphp
+
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
